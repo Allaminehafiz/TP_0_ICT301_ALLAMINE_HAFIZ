afecd7bf4e01d93c7bb27baea23014ef2ca4a8bd
Allaminehafiz <allaminhafiz68@gmail.com>
2025-12-25 22:06:00 +0100

    3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -82,5 +82,16 @@ public class Main {
         AreaCalculator2 calculator = new AreaCalculator2();
         System.out.println("Area = " + calculator.calculateArea(myShape));
         */
+
+        Rectangle rectangle = new Rectangle();
+         rectangle.setWidth(5);
+         rectangle.setHeight(4); // Attend une aire de 20
+         System.out.println("Aire du Rectagle ="+rectangle.getArea());
+
+        Rectangle rectangle1 = new Rectangle.Square();
+         rectangle1.setWidth(5);
+         rectangle1.setHeight(4); // Attend une aire de 20, mais obtient 16 (4x4)
+         System.out.println("Aire du Carré ="+rectangle1.getArea()); // Résultat inattendu }
+
     }
 }
